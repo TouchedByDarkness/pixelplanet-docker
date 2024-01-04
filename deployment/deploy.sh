467604051f72c62c0adbfa17d1741f6c0eb93166
HF <hf@pixelplanet.fun>
2024-01-04 15:19:36 +0100

    add restart command to deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,3 +2,4 @@
 tar --exclude='./dist/tiles' --exclude='./dist/log' -cvJf /tmp/ppfun.tar.xz ./dist
 scp /tmp/ppfun.tar.xz pixelplanet:/tmp/
 rm /tmp/ppfun.tar.xz
+ssh pixelplanet ./deploy-from-temp.sh
